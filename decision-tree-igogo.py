e57dfdd1548b8f0cb700d7e7f4a869686642558f
Igor Gorovyy <igorgorovoy@ukr.net>
2023-05-13 22:24:03 +0200

    change decision logic

--- decision-tree-igogo.py
+++ decision-tree-igogo.py
@@ -23,10 +23,18 @@ def classify_book(decision_tree):
     # Loop through the questions until we reach a genre
     while isinstance(node, dict):
         question = list(node.keys())[0]
-        answer = input(question + " ")
-        node = node[answer]
+        answer_options = list(node[question].keys())
+        answer = input(f"{question} ({'/'.join(answer_options)}) ")
+
+        # Check if answer is valid
+        while answer not in answer_options:
+            print(f"Invalid answer '{answer}'. Please choose from the following options: {', '.join(answer_options)}")
+            answer = input(f"{question} ({'/'.join(answer_options)}) ")
+
+        node = node[question][answer]
 
     # Print the final genre
     print("Genre:", node)
+
 
 classify_book(decision_tree)
